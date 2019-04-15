7c947c07cac2bd35bffab3345b4d56e81f187497
Nico Petrakis <nico.petrakis@me.com>
2019-04-15 08:05:41 +0200

    Update theming script

--- terminal.sh
+++ terminal.sh
@@ -6,15 +6,26 @@ echo "Zsh"
 brew install zsh
 
 echo "Oh My Zsh"
+rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # These things are backuped because of mackup
 
 # install autosuggestions plugin
 # https://github.com/zsh-users/zsh-autosuggestions
-# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # install highlighting plugin
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Theme: https://github.com/bhilburn/powerlevel9k
+brew tap sambadevi/powerlevel9k
+brew install powerlevel9k
+echo "source /usr/local/opt/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+
+# Fonts
+https://github.com/powerline/fonts/tree/master/SourceCodePro
+# Change them in iTerm
+
+chsh -s /bin/zsh
+
